145eff1929ddfb3f548e002edd3dde67721beb3d
Aparyan <aryan.prakash.ug20@nsut.ac.in>
2022-06-29 16:33:51 +0530

    'Netflix'

---
+++ src/Data.jsx
@@ -0,0 +1,40 @@
+const Data = [
+    {
+  imgsrc:"https://i.pinimg.com/736x/93/56/d8/9356d82490277928bc929a4f2c9a245e.jpg" ,
+  title:"A Netflix Original Series" ,
+  series_name:"DARK",
+  link:"https://www.netflix.com/in/title/80100172",
+    },
+    {
+        imgsrc:"https://i.pinimg.com/originals/95/64/cc/9564cc5d91b06c8705bb95ea22b9e940.png",
+  title:"A Netflix Original Series",
+  series_name:"STRANGER THINGS",
+  link:"https://www.netflix.com/in/title/80057281",
+    },
+    {
+        imgsrc:"https://www.themoviedb.org/t/p/original/8jUlLqqPVkO7tRtZsGYjWteJJeR.jpg",
+  title:"A Netflix Original Series",
+  series_name:"All Of Us Are Dead",
+  link:"https://www.netflix.com/in/title/81237994",
+    },
+    {
+        imgsrc:"https://cdn.europosters.eu/image/750/posters/squid-game-crowd-i120012.jpg",
+  title:"A Netflix Original Series",
+  series_name:"SQUID GAMES",
+  link:"https://www.netflix.com/in/title/81040344",
+    },
+    {
+        imgsrc:"https://www.themoviedb.org/t/p/original/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg",
+  title:"A Netflix Original Series",
+  series_name:"MONEY HEIST",
+  link:"https://www.netflix.com/in/title/80192098",
+    },
+    {
+        imgsrc: "https://wallpaperaccess.com/full/6520910.jpg",
+        title:"A Netflix Original Series",
+        series_name:"Peaky Blinders",
+        link:"https://www.netflix.com/in/title/80002479",
+    }
+]
+
+export default Data;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./Card";
+import "./index.css";
+import Data from "./Data";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <>
+  <h1 className="heading_style"> Top Netflix Series</h1>
+  {Data.map((val) => {
+    return (
+      <Card
+      imgsrc = {val.imgsrc}
+      title = {val.title}
+      series_name = {val.series_name}
+      link = {val.link} />
+    );
+  })}
+  </>
+,document.getElementById('root'));
